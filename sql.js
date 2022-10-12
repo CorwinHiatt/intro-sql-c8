29bd2d772a96a6b3c7a98cc9f171cfef98f5c2c1
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-12 16:49:16 -0400

    3

--- sql.js
+++ sql.js
@@ -1,2 +1,42 @@
-import pg from 'pg';
-const {Client} = pg;
+// import pg, { Pool } from 'pg';
+// import { database, password } from 'pg/lib/defaults';
+// import client from 'pg/lib/native/client';
+// const {Client,pool} = pg;
+// await client.connect()
+
+// const pool = new Pool(
+//     {
+//         host: "sql.bocacode.com"
+//         database: "bocacode"
+//         user: "bocacode"
+//         password: "bocacode"
+//         port: 5432
+
+//     }
+// )
+
+import pg from "pg"
+const { Client, Pool } = pg
+
+const pool = new Pool(
+    {
+        host: "sql.bocacode.com",
+        database: "bocacode",
+        user: "bocacode",
+        password: "bocacode",
+        port: 5432
+    }
+)
+
+const query = `select * from 
+customers order by 
+customers.customerid`
+
+pool.query(query, (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    console.table(data.rows)
+    pool.end()
+})
