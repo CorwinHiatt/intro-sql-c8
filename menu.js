8c827f0e4fe934512128fd288ebf86dabefd1991
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-13 16:56:39 -0400

    7

--- menu.js
+++ menu.js
@@ -16,22 +16,36 @@ async function runQuery(query) {
 
 while (true) {
     const answer = readline.question("run what?")
+    let query = ''
+    let results = ''
     //1 select * from customers 
     switch (answer) {
         case "1":
-            const query = `select * from customers `
-            const rows = await runQuery(query)
-            console.table(rows.rows)
+            query = `SELECT first_name,last_name FROM customers`;
+            const rows = await runQuery(query);
+            console.table(rows.rows);
             break
         case '2':
+            query = `INSERT INTO customers(
+                first_name, "last_name", phone, email)
+               VALUES ('Tmmybob', 'Aligarky', '9712679344', 'corwin@brosiphcacode.com');`
+            results = await runQuery(query)
+            console.log('user added')
             break;
-            //2 add a new customer 
+        //2 add a new customer 
         case '3':
+            query =`UPDATE public.customers
+            SET  first_name='CORy', last_name='hello',
+            email='jon@brosiphcacode.com' , phone= '888267777', 
+            updated_at=NOW()
+            WHERE customer_id=1` 
+            results = await runQuery(query)
+            console.log('customer updated')
             break;
-            //3 update that customer 
+        //3 update that customer 
 
-            default:
-                console.log('wat!!!')
-        }
+        default:
+            console.log('wat!!!')
+            break;
     }
-    
+}
