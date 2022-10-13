781058e724934598034585d53dd9a339b338dad0
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-13 16:20:11 -0400

    5

--- menu.js
+++ menu.js
@@ -6,17 +6,32 @@ import { creds } from "./credential2.js"
 
 const pool = new Pool(creds);
 
-const query = `select * from customers `
+//const query = `select * from customers `
 
-pool.query(query, (err, data) => {
-    if (err) {
-        console.error(err)
-        return
+
+async function runQuery(query) {
+    return await pool.query(query)
+
+}
+
+while (true) {
+    const answer = readline.question("run what?")
+    //1 select * from customers 
+    switch (answer) {
+        case "1":
+            const query = `select * from customers `
+            const rows = await runQuery(query)
+            console.table(rows.rows)
+            break
+        case '2':
+            break;
+            //2 add a new customer 
+        case '3':
+            break;
+            //3 update that customer 
+
+            default:
+                console.log('wat!!!')
+        }
     }
-    console.log(data)
-    console.table(data.rows)
-    pool.end()
-})
-
-
-
+    
